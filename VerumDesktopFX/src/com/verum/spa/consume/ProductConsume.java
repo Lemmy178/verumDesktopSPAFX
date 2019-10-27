da716a96b5988b8747f97be08796c0599af6362c
Ruben <vuelo.privado2@gmail.com>
2019-10-27 08:45:11 -0600

    Agregando sidebar

---
+++ VerumDesktopFX/src/com/verum/spa/consume/ProductConsume.java
@@ -0,0 +1,55 @@
+package com.verum.spa.consume;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+public class ProductConsume {
+
+    public static String addProduct(String proName, String proBrand, double proPrice) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080/SpaSmartTech/api/product/add");
+        String aux = String.valueOf(proPrice);
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        Map<String, String> valeurs = new HashMap<String, String>();
+        valeurs.put("proName", proName);
+        valeurs.put("proBrand", proBrand);
+        valeurs.put("proPrice", aux);
+
+        String json = gson.toJson(valeurs);
+        String response = null;
+        response = target.request()
+                .post(Entity.entity(json, MediaType.APPLICATION_JSON),
+                        String.class);
+        return response;
+    }
+
+    public String listProduct() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080/SpaSmartTech/api/product/productList");
+        return target.request(MediaType.APPLICATION_JSON).get(String.class);
+    }
+
+//    private static void postUsingRawJSON(WebTarget target) {
+//        String customer = ClientUtil.createCustomerInJSON("Alyssa William",
+//                 "1021 Hweitt Street",
+//                 "343-343-3433");
+//        String response = target.request()
+//                .post(Entity.entity(customer, MediaType.APPLICATION_JSON),
+//                         String.class);
+//        System.out.println("customer created with id: " + response);
+//
+//        //get the new customer
+//        getCustomerById(target, response);
+//
+//    }
+}
