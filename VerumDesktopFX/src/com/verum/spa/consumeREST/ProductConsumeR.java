92b7755ca6b3aadea25007a68d12e0381c98df1e
Ruben <vuelo.privado2@gmail.com>
2019-10-28 08:00:07 -0600

    Ya inserta datos

--- VerumDesktopFX/src/com/verum/spa/consumeREST/ProductConsumeR.java
+++ VerumDesktopFX/src/com/verum/spa/consumeREST/ProductConsumeR.java
@@ -10,7 +10,7 @@
 
                 http://localhost:8080/VerumRESTSpa/api/product
  *===========================================================================*/
-package com.verum.spa.consume;
+package com.verum.spa.consumeREST;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -21,30 +21,29 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
-public class ProductConsume {
+public class ProductConsumeR {
 
-    //Global variables
-    Client client = ClientBuilder.newClient();
-    WebTarget target;
-    String values = "";
-    GsonBuilder builder = new GsonBuilder();
-    Gson gson = builder.create();
-    Product producto;
+    //global variables
+    private Client client = ClientBuilder.newClient();
+    private WebTarget target;
+    private String values = "";
+    private GsonBuilder builder = new GsonBuilder();
+    private Gson gson = builder.create();
+    private Product pro = new Product();
 
-    
-    //POST
-    public String addProduct(String proName, String proBrand, double proPrice) {
-        
+    //ADD
+    public String addPro(String proNam, String proBra, double proPri) {
+        pro.setProName(proNam);
+        pro.setProBrand(proBra);
+        pro.setProPrice(proPri);
+
         target = client.
                 target("http://localhost:8080/VerumRESTSpa/api/product/add");
-        
-        producto = new Product(proName, proBrand, proPrice);
-
         values = target.request()
-                .post(Entity.entity(producto, MediaType.APPLICATION_JSON),
+                .post(Entity.entity(pro, MediaType.APPLICATION_JSON),
                         String.class);
-        
         return values;
+
     }
 
     //GET
@@ -52,9 +51,7 @@ public class ProductConsume {
         //Funcionando
         target = client.
                 target("http://localhost:8080/VerumRESTSpa/api/product/productList");
-        
         values = target.request().get(String.class);
-        
         return values;
     }
 
