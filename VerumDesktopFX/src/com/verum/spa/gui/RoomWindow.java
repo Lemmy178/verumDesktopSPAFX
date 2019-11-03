3c724da973ec41c9bea1aaa416f162a43df7221d
RichardJohnRx <the.cool.guy492@gmail.com>
2019-11-03 16:11:24 -0600

    Se implemento 'Room' en el main

--- VerumDesktopFX/src/com/verum/spa/gui/RoomWindow.java
+++ VerumDesktopFX/src/com/verum/spa/gui/RoomWindow.java
@@ -8,23 +8,19 @@
  *===========================================================================*/
 package com.verum.spa.gui;
 
-import com.jfoenix.controls.JFXButton;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeTableView;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -35,11 +31,18 @@ public class RoomWindow implements Initializable {
     Scene scene;
     
     @FXML TreeTableView trtRoom;
+    
     @FXML TextField txtRoomName;
     @FXML TextArea txaRoomDesc;
     @FXML TextField txtRoomStatus;
     
     @FXML ImageView imgvRoomPhoto;
+    
+    @FXML Button btnAdd;
+    @FXML Button btnUpdate;
+    @FXML Button btnDelete;
+    
+    @FXML VBox vBox;
     
     Parent root = null;
 
@@ -47,7 +50,23 @@ public class RoomWindow implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         
     }
-
+    
+    public void addListeners() {
+        for (Node node : vBox.getChildren()) {
+            if (node.getAccessibleText() != null) {
+                node.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, (e) -> {
+                    switch (node.getAccessibleText()) {
+                        case "add":
+                            break;
+                        case "update":
+                            break;
+                        case "delete":
+                            break;
+                    }
+                });
+            }
+        }
+    }
     
 
     
